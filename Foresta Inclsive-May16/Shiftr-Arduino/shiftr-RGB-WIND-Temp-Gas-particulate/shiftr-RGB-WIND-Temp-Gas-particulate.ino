b33208a4f634f8451982e55e9a645bcda17846d2
Buddadweet <janetingley@gmail.com>
2020-03-10 15:58:51 -0400

    one large sketch communicating via ethernet

--- Foresta Inclsive-May16/Shiftr-Arduino/shiftr-RGB-WIND-Temp-Gas-particulate/shiftr-RGB-WIND-Temp-Gas-particulate.ino
+++ Foresta Inclsive-May16/Shiftr-Arduino/shiftr-RGB-WIND-Temp-Gas-particulate/shiftr-RGB-WIND-Temp-Gas-particulate.ino
@@ -31,7 +31,11 @@ Anemometer wind sensor -- red to + of power source - 12vdc is fine.
 #include "Adafruit_TCS34725.h" //RGB colour sensor
 #include <Adafruit_Sensor.h>  // BME - Temp/pressure sensor
 #include <Adafruit_BME280.h> // BME - Temp/pressure sensor
-#include "Adafruit_CCS811.h" // Gas sensor
+#include "Adafruit_CCS811.h" //CCS811 code - Co2 and TVOC
+
+//https://learn.adafruit.com/pm25-air-quality-sensor/arduino-code
+#include <SoftwareSerial.h>
+SoftwareSerial pmsSerial(2, 3);
 
 #define BME_SCK 13 // BME - Temp/pressure sensor
 #define BME_MISO 12 // BME - Temp/pressure sensor
@@ -51,52 +55,63 @@ EthernetClient net;
 MQTTClient client;
 Adafruit_BME280 bme; // I2C - BME - Temp/pressure sensor
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X); //RGB colour sensor
-Adafruit_CCS811 ccs; // Gas sensor
+Adafruit_CCS811 ccs; //CCS811 code - Co2 and TVOC
 
-int sensorPin = A0;    // select the input pin for the wind sensor
-int windsensorValue = 0;  // variable to store the value coming from the wind sensor
+struct pms5003data { //particulate
+  uint16_t framelen;
+  uint16_t pm10_standard, pm25_standard, pm100_standard;
+  uint16_t pm10_env, pm25_env, pm100_env;
+  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+  uint16_t unused;
+  uint16_t checksum;
+}; 
+
+struct pms5003data data;
 
 unsigned long fastDelayTime = 1000;
 unsigned long slowDelayTime = 10000;
 unsigned long previousFastMillis = 0;
 unsigned long previousSlowMillis = 0;
+int sensorPin = A0;    // select the input pin for the wind sensor
+int windsensorValue = 0;  // variable to store the value coming from the wind sensor
+
 //int moistureReading = 0;   // This is for the received sensor value sent by shiftr
 //int lightReading = 0;   // This is for the received sensor value sent by shiftr
 //int temperatureReading = 0;   // This is for the received sensor value sent by shiftr
 
-int moistureThreshold = 470;  // This holds the threshold for the soil - change here and reupload
-int valveTime = 4000;  // The amount of time between each time the valve is actuated (seconds)
+//int moistureThreshold = 470;  // This holds the threshold for the soil - change here and reupload
+//int valveTime = 4000;  // The amount of time between each time the valve is actuated (seconds)
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200); // this is now faster for particulate sensor
 
+        // sensor baud rate is 9600 //PM2.5
+        pmsSerial.begin(9600);
+        
     tcs.begin(); //RGB colour sensor
     bme.begin();  //Pressure/Temp sensor
     ccs.begin(); // gas sensor
 
 // from DhcpAddressPrinter           
      // start the Ethernet connection:
-//                    Serial.println("Initialize Ethernet with DHCP:");
+//    Serial.println("Initialize Ethernet with DHCP:");
       if (Ethernet.begin(mac) == 0) {
-//                      Serial.println("Failed to configure Ethernet using DHCP");
+//      Serial.println("Failed to configure Ethernet using DHCP");
         if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-//                        Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+//      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
         } else if (Ethernet.linkStatus() == LinkOFF) {
-//                        Serial.println("Ethernet cable is not connected.");
+//      Serial.println("Ethernet cable is not connected.");
         }
-        // no point in carrying on, so do nothing forevermore:
+       // no point in carrying on, so do nothing forevermore:
        while (true) {
           delay(1);
         }
-      }
-      
-      // print your local IP address:
-//    Serial.print("My IP address: ");
-//    Serial.println(Ethernet.localIP());
-//    end from DhcpAddressPrinter
+      }      
+   // print your local IP address:
+   // Serial.print("My IP address: ");
+   // Serial.println(Ethernet.localIP());
+   // end from DhcpAddressPrinter
     
-    // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
-    // You need to set the IP address directly.
     client.begin("broker.shiftr.io", net);
     //client.onMessage(messageReceived);  // call this function (message received) whenever there is a message
     connect();
@@ -109,8 +124,6 @@ void loop() {
   if (!client.connected()) {
     connect();
   }
-
-
   // This sends the sensor data that needs to be sent quickly to shiftr - ie//wind
     unsigned long currentMillis = millis();
     if (currentMillis - previousFastMillis >= fastDelayTime) {
@@ -126,8 +139,8 @@ void loop() {
         if (currentMillis - previousSlowMillis >= slowDelayTime) {
           previousSlowMillis = currentMillis;
 
-        //this converts the RGB colour sensor data 
-        /*float red, green, blue; //taken from the other sketch to transform RGB values
+        /*//this converts the RGB colour sensor data 
+        float red, green, blue; //taken from the other sketch to transform RGB values
         uint16_t r, g, b, c, colorTemp, lux;
         tcs.getRawData(&r, &g, &b, &c);
         tcs.getRGB(&red, &green, &blue);//taken from the other sketch to transform RGB values
@@ -138,7 +151,8 @@ void loop() {
         // print sensor values only when necessary - else look at shiftr
         client.publish("/colourTemp", String(colorTemp));
         //Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-        client.publish("/Lux", String(lux));*/
+        client.publish("/Lux", String(lux));
+        //Serial.print("Lux: "); Serial.println(lux, DEC);*/
 
         //Gas/VOC sensor
         if(ccs.available()){
@@ -149,8 +163,7 @@ void loop() {
         //Serial.print("VOC: "); Serial.print(ccs.getTVOC()); Serial.println("");
           }
         }    
-  
-        //Serial.print("Lux: "); Serial.print(lux, DEC); Serial.println("");
+        /*
         //BME sensor - Humidity/temp/pressure
         client.publish("/Temp-degree", String(int(bme.readTemperature())));
         //Serial.print("Temp-degree: "); Serial.print(bme.readTemperature()); Serial.println("");
@@ -161,15 +174,84 @@ void loop() {
         client.publish("/Humid", String(int(bme.readHumidity())));
         //Serial.print("Humid: "); Serial.print(bme.readHumidity()); Serial.println("");
 
-        /* Make processing sketch for this function
+        Make processing sketch for this function
         client.publish("/NewThreshold", String(moistureThreshold)); // sending to shiftr client.publish("NewThreshold", String(moistureThreshold)); // sending to shiftr
         //Serial.print("moistureThreshold value : "); Serial.println(moistureThreshold);
         client.publish("/ValveTime", String(valveTime));
         //Serial.print("valveTime value : "); Serial.println(valveTime);
-         */
+         */     
+        
+          if (readPMSdata(&pmsSerial)) {
+            // reading data was successful!
+            client.publish("/Particles.3", String(data.particles_03um)); 
+            //Serial.print("Particles > 0.3um / 0.1L air:"); Serial.println(data.particles_03um);
+            client.publish("/Particles.5", String(data.particles_05um));
+            //Serial.print("Particles > 0.5um / 0.1L air:"); Serial.println(data.particles_05um);
+            client.publish("/Particles1", String(data.particles_10um));
+            //Serial.print("Particles > 1.0um / 0.1L air:"); Serial.println(data.particles_10um);
+            client.publish("/Particles2.5", String(data.particles_25um));
+            //Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
+            client.publish("/Particles5", String(data.particles_50um));
+            //Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
+            client.publish("/Particles10", String(data.particles_100um));
+            //Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
+            //Serial.println("---------------------------------------");
+          } 
     }
-  }
+  } 
   DhcpAddress();
+}
+
+///----------------------particle
+boolean readPMSdata(Stream *s) {
+  // Read a byte at a time until we get to the special '0x42' start-byte
+  while (s->available() && s->peek() != 0x42) {
+    s->read();
+  }
+
+  if (! s->available()) {
+    Serial.println("no data");
+    return false;
+  }
+  
+  // Now read all 32 bytes
+  if (s->available() < 32) {
+    Serial.println("< 32 bytes available");
+    return false;
+  }
+    
+  uint8_t buffer[32];    
+  uint16_t sum = 0;
+  s->readBytes(buffer, 32);
+
+  // get checksum ready
+  for (uint8_t i=0; i<30; i++) {
+    sum += buffer[i];
+  }
+
+  /* debugging
+  for (uint8_t i=2; i<32; i++) {
+    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
+  }
+  Serial.println();
+  */
+  
+  // The data comes in endian'd, this solves it so it works on all platforms
+  uint16_t buffer_u16[15];
+  for (uint8_t i=0; i<15; i++) {
+    buffer_u16[i] = buffer[2 + i*2 + 1];
+    buffer_u16[i] += (buffer[2 + i*2] << 8);
+  }
+
+  // put it into a nice struct :)
+  memcpy((void *)&data, (void *)buffer_u16, 30);
+
+  if (sum != data.checksum) {
+    Serial.println("Checksum failure");
+    return false;
+  }
+  // success!
+  return true;
 }
 
 void connect() {
