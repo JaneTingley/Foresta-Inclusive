375129be1ff07894b19876200d20e9645969e317
Buddadweet <janetingley@gmail.com>
2020-03-20 14:04:21 -0400

    updated wifi sketch

--- Foresta Inclsive-May16/Shiftr-Arduino/WIFI/wifishiftrjane/arduino_secrets.h
+++
@@ -1,6 +0,0 @@
-#define SECRET_SSID "Evol"
-#define SECRET_PASS "5138loove"
-
-#define MQTT_NAMESPACE "Foresta-Inclusive-WiFi" //"kintel-wifi"
-#define MQTT_USERNAME "83aa4496" //"66ddedc6"
-#define MQTT_PASSWORD "02ffd19115bcd0ed" //"fe2d57914c828f0b"
