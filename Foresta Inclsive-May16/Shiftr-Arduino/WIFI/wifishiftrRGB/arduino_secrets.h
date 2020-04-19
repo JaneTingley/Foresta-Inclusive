9d10598fca4dfee7f3be5cecd42cfc85fc9e8d3b
Buddadweet <janetingley@gmail.com>
2020-04-19 12:17:49 -0400

    Update arduino_secrets.h

--- Foresta Inclsive-May16/Shiftr-Arduino/WIFI/wifishiftrRGB/arduino_secrets.h
+++ Foresta Inclsive-May16/Shiftr-Arduino/WIFI/wifishiftrRGB/arduino_secrets.h
@@ -2,5 +2,5 @@
 #define SECRET_PASS "5138loove"
 
 #define MQTT_NAMESPACE "Foresta-Inclusive-WiFiRGB" //"kintel-wifi"
-#define MQTT_USERNAME "83aa4496" //"66ddedc6"
-#define MQTT_PASSWORD "02ffd19115bcd0ed" //"fe2d57914c828f0b"
+#define MQTT_USERNAME "c9f6b6d3" //"66ddedc6"
+#define MQTT_PASSWORD "055ad1c0d8de9605" //"fe2d57914c828f0b"
