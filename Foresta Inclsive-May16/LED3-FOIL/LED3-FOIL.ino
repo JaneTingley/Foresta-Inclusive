e0e0fbf3f3131b9270aa573792a898fd8ffb62f2
Buddadweet <janetingley@gmail.com>
2020-04-21 14:34:21 -0400

    Update LED3-FOIL.ino

--- Foresta Inclsive-May16/LED3-FOIL/LED3-FOIL.ino
+++ Foresta Inclsive-May16/LED3-FOIL/LED3-FOIL.ino
@@ -6,8 +6,8 @@
 This program blinks the 2 high powered LEDs - headlight, and foil at 2 second intervals
 */
 
-int LED_BLUE=8; //declares pin variables
-int LED_RED=7;
+int LED_WHITE=8; //declares pin variables - in this case white
+int LED_AMBER=7; // in this case amber
 int LED_HEADLIGHT=6;
 int FOIL=9;
 
@@ -17,8 +17,8 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   
   
-  pinMode(LED_BLUE, OUTPUT); //LED_BLUE  - declares pins as output
-  pinMode(LED_RED, OUTPUT); //LED_RED
+  pinMode(LED_WHITE, OUTPUT); //LED_WHITE  - declares pins as output
+  pinMode(LED_AMBER, OUTPUT); //LED_AMBER
   pinMode(LED_HEADLIGHT, OUTPUT); //LED_HEADLIGHT
   pinMode(FOIL, OUTPUT); //FOIL
 
@@ -27,18 +27,24 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BLUE, LOW);  // these leds start high - so sets to low
-  digitalWrite(LED_RED, LOW); 
+  digitalWrite(LED_WHITE, LOW);  // these leds start high - so sets to low
+  digitalWrite(LED_AMBER, LOW); 
 
   
-  digitalWrite(LED_BLUE, !HIGH);   // turn the LED on (LOW turns these LEDs on - but the ! before the HIGH flips = LOW)
-  digitalWrite(LED_RED, !HIGH);   // turn the LED on (LOW turns these LEDs on - but the ! before the HIGH flips = LOW)
-  digitalWrite(LED_HEADLIGHT, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(FOIL, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
-  digitalWrite(LED_BLUE, !LOW);    // turn the LED off by making the voltage !LOW
-  digitalWrite(LED_RED, !LOW);    // turn the LED off by making the voltage !LOW
-  digitalWrite(LED_HEADLIGHT, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(FOIL, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);                       // wait for a second
+  digitalWrite(LED_WHITE, LOW);   // turn the LED on 
+  digitalWrite(LED_AMBER, HIGH);   // keep the LED off 
+  digitalWrite(LED_HEADLIGHT, LOW);   // turn off headlight
+  digitalWrite(FOIL, HIGH);   // turn high = clear
+  delay(10000);                       // wait for a second
+  digitalWrite(FOIL, LOW);    // turn the foil low = opaque
+  delay(4000);                       // wait for a second
+  
+  digitalWrite(LED_WHITE, HIGH);    // turn the LED off 
+  digitalWrite(LED_AMBER, LOW);    // turn the LED on 
+
+  delay(10000);                       // wait for a second
+
+  digitalWrite(LED_HEADLIGHT, HIGH);    // turn on headlight
+
+  delay(5000); 
 }
